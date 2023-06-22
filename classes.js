f293b60f947721d8fe4e6f980a4ff1c4e6e834f1
Shara5904 <128625731+Shara5904@users.noreply.github.com>
2023-06-22 15:01:22 -0300

    all

---
+++ classes.js
@@ -0,0 +1,8 @@
+class produto {
+    constructor(link, nome, preco) {
+        this.link = link;
+        this.nome = nome;
+        this.nome = preco;
+    }
+}
+
---
+++ script.js
@@ -0,0 +1,17 @@
+let produto1 = new produto(
+    "https://imgnike-a.akamaihd.net/1920x1920/01113751.jpg",
+    "Tênis Air Force Maculino Branco",
+    "999,99"
+)
+
+let produto2 = new produto(
+    "https://imgnike-a.akamaihd.net/1920x1920/00791251.jpg",
+    "Tênis Air Force Feminino Branco",
+    "999,99"
+)
+
+let produto3 = new produto(
+    "https://imgnike-a.akamaihd.net/1920x1920/00791251.jpg",
+    "Tênis Air Force Feminino Branco",
+    "699,99"
+)
